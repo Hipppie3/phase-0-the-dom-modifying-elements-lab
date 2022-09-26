0c274a9aa281d13bbd475cbe1720afbb702559fb
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-09-25 19:26:54 -0600

    commit tree 691ec7ceb742c43ae7fa8cc71a7bd0df718da0e6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+const main = document.querySelector('main')
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Daniel is the champion";
